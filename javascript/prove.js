581257e6197a8267e916636e10229c658986451e
elmoriconi <ele.9.9@virgilio.it>
2024-07-02 13:56:08 +0200

    non viene

--- javascript/prove.js
+++ javascript/prove.js
@@ -1,7 +1,63 @@
-console.log("ciao");
-
 const prompt = require('prompt-sync')();
-
-const str = prompt('Enter a string: ');
+/*console.log("ciao");
+function capitalizeFirstLetter(string)
+{
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+var str = prompt('Enter a string: ');
 console.log("Hai inserito " + str);
+str = capitalizeFirstLetter(str);
+console.log("Hai inserito " + str);
+process.exit(0);
+*/
+
+//Inserisci primo numero e salvi su sPrimoNumero
+//Inserisci secnondo numero
+//Stampi a schermo la somma
+
+function mioParseInt(string) {
+    for (i = 0; i <= string.length; i++) {
+        if ((string.charAt(i)>='0') || (string.charAt(i)<='9')) {
+        }
+        else {
+            return false
+        }
+    }
+    return true
+}
+
+/*function VerificaFallita(numero) 
+{
+    bEsito = mioParseInt(numero);
+    if (bEsito == true)
+        return parseInt(numero);
+    for(i = 0; i <= 3; i++)
+    {
+        numero = prompt('Attenzione numero errato, riprova: ');
+        bEsito = mioParseInt(numero);
+        if (bEsito == true)
+            return parseInt(numero);
+    }
+    return null;
+}*/
+
+var sPrimoNumero = prompt('Enter first number: ');
+
+flag = mioParseInt(sPrimoNumero)
+if (flag == true) {
+    sPrimoNumero = parseInt(sPrimoNumero);
+}
+else {
+    console.log("Hai inserito caratteri non numerici");
+}
+var sSecondoNumero = prompt('Enter second number: ');
+flag = mioParseInt(sSecondoNumero)
+if (flag == true) {
+    sSecondoNumero = parseInt(sSecondoNumero);
+}
+else {
+    console.log("Hai inserito caratteri non numerici");
+}
+var sSomma = sPrimoNumero + sSecondoNumero
+console.log("Somma " + sSomma);
 process.exit(0);
