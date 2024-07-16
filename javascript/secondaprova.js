25c3e8c9a1a9ad43d85bf82eef70c9b715f883e3
elmoriconi <ele.9.9@virgilio.it>
2024-07-16 10:26:52 +0200

    javascript

---
+++ javascript/secondaprova.js
@@ -0,0 +1,31 @@
+/*var A;
+var B;
+A = 3;
+B = "buongiorno a tutti";
+console.log("Il tipo della var A e' " + typeof(A) + "\n");
+console.log("Il tipo della var B e' " + typeof (B) + "\n");*/
+
+var C = 13;
+console.log("Il tipo della var C e' " + typeof(C) + "\n");
+C = "che bella giornata"
+console.log("Il tipo della var C e' " + typeof(C) + "\n");
+
+var iPosizione;
+var sMiaStringa;
+sMiaStringa = "pazperinoz"
+iPosizione = sMiaStringa.indexOf("z");
+if((iPosizione == -1) || (iPosizione > 3))
+						console.log("La z non c'e' nei primi 4 caratteri")
+else
+	console.log("La z c'e' nei primi 4 caratteri");
+
+var iPosizione;
+var sMiaStringa;
+sMiaStringa = "giovanni"
+iPosizione = sMiaStringa.indexOf("f");
+if(iPosizione == -1)
+                    console.log("La f non c'è")
+else
+    console.log("La f c'è");
+
+
